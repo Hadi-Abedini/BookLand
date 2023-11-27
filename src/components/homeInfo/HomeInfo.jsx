70b72e4e95a5b2c517a0e5cfbc8429037faf1089
Hadi-Abedini <hadi.z.abedini@gmail.com>
2023-11-27 11:01:06 +0330

    init info sections

---
+++ src/components/homeInfo/HomeInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function HomeInfo() {
+  return <div>HomeInfo</div>;
+}
+
+export default HomeInfo;
---
+++ src/components/homeInfo/InfoCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function InfoCard() {
+  return <div>InfoCard</div>;
+}
+
+export default InfoCard;
