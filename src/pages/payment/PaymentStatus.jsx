fa702390adbc5ad73b66097d84f0b8fa181edb79
Hadi-Abedini <hadi.z.abedini@gmail.com>
2023-12-24 15:29:13 +0330

    fix style

--- src/pages/payment/PaymentStatus.jsx
+++ src/pages/payment/PaymentStatus.jsx
@@ -6,14 +6,14 @@ function PaymentStatus() {
   return status === "success" ? (
     <div className="w-full h-[100vh] flex gap-4 justify-center items-center">
       <i
-        class="fa fa-check-circle"
+        className="fa fa-check-circle"
         style={{ fontSize: 100 + "px", color: "green" }}></i>
       <p className="text-lg">پرداخت با موفقیت انجام شد منتظر تماس ما باشید</p>
     </div>
   ) : (
     <div className="w-full h-[100vh] flex gap-4 justify-center items-center">
       <i
-        class="fa fa-exclamation-circle"
+        className="fa fa-exclamation-circle"
         style={{ fontSize: 100 + "px", color: "red" }}></i>
 
       <p className="text-lg">پرداخت با شکست مواجه شد سفارش شما در انتظار پرداخت است</p>
