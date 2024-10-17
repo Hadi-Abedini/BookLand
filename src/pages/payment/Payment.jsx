1dc3f175915f2f2950336d3b6c0ee254f38b54ca
Hadi Abedini <99591486+Hadi-Abedini@users.noreply.github.com>
2024-10-17 20:04:33 +0330

    update payment page

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -16,13 +16,17 @@ function Payment() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <div className="w-full h-[100vh] flex gap-9 justify-center items-center">
-        <PayBtn />
-        <Link
-          to={"/payment-status/failure"}
-          className="bg-red-700 py-3 px-7 rounded-lg text-white hover:bg-red-400">
-          انصراف
-        </Link>
+      <div className="w-full flex flex-col gap-4 items-center content-center">
+        <img className="w-[80%] mh-[50%]" src="src/assets/banner/payment.png" />
+        <div className="flex gap-9 justify-center items-center">
+          <PayBtn />
+          <Link
+            to={"/payment-status/failure"}
+            className="bg-red-700 py-3 px-7 rounded-lg text-white hover:bg-red-400"
+          >
+            انصراف
+          </Link>
+        </div>
       </div>
     </QueryClientProvider>
   );
